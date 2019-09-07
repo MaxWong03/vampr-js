d06865d5d67c07767c750d0bd6bcfd7ec5e00a54
MaxWong03 <madmaxmmajournal@gmail.com>
2019-09-07 15:14:39 -0700

    a

--- vampire.js
+++ vampire.js
@@ -10,35 +10,56 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
-
+    if (!this.creator) return 0;
+    else return 1 + this.creator.numberOfVampiresFromOriginal;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
+  /**
+   * This checks is the passed in vampire is more senior than the vampire that is calling the function
+   */
   isMoreSeniorThan(vampire) {
+    if (this.numberOfVampiresFromOriginal > vampire.numberOfVampiresFromOriginal) return false; //if im more senior
+    return true;
+  }
+
+  // Returns the vampire object with that name, or null if no vampire exists with that name
+  vampireWithName(name) {
 
   }
 
-  /** Stretch **/
+  // Returns the total number of vampires that exist
+  get totalDescendents() {
 
-  // Returns the closest common ancestor of two vampires.
-  // The closest common anscestor should be the more senior vampire if a direct ancestor is used.
-  // For example:
-  // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
-  // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
-  closestCommonAncestor(vampire) {
+  }
+
+  // Returns an array of all the vampires that were converted after 1980
+  get allMillennialVampires() {
 
   }
 }
+
+/** Stretch **/
+
+// Returns the closest common ancestor of two vampires.
+// The closest common anscestor should be the more senior vampire if a direct ancestor is used.
+// For example:
+// * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
+// * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
+
+
+
 
 module.exports = Vampire;
 
